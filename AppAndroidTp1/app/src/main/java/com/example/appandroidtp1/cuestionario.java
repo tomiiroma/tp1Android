77e304e81e90f203ae70806e48cc5de50ae01cdc
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-09-24 14:11:05 -0300

    cambios en las preguntas y metodos

--- AppAndroidTp1/app/src/main/java/com/example/appandroidtp1/cuestionario.java
+++ AppAndroidTp1/app/src/main/java/com/example/appandroidtp1/cuestionario.java
@@ -19,15 +19,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class cuestionario extends AppCompatActivity {
 
-
     LinkedList<Pregunta> preguntas;
-
     RadioButton rButton1,rButton2,rButton3,rButton4;
     TextView txtPreguntaNumero,txtPreguntaCompleta, textViewTuPuntaje;
     Button btnConfirmar;
     String nombreRecibido;
-
-    int nPregunta = 1;
+    int nPregunta = 0;
     int puntajeFinal = 0;
 
     @Override
@@ -45,13 +42,12 @@ public class cuestionario extends AppCompatActivity {
                         "Diego Maradona (Argentina)",
                         "Lionel Messi (Argentina)"}, 0));
 
-
         preguntas.add(new Pregunta("¿Cómo se llamó la mascota de la Copa Mundial de 1982 en España?",
-                new String[]{"Ronaldo Nazario (Brasil)",
+                new String[]{
+                        "Zakumi",
                         "Fuleco",
                         "Pique",
                         "Naranjito"}, 3));
-
 
         preguntas.add(new Pregunta("¿En qué año se celebró la primera Copa Mundial de Fútbol?",
                 new String[]{
@@ -59,8 +55,6 @@ public class cuestionario extends AppCompatActivity {
                         "1960",
                         "1956",
                         "1938"}, 0));
-
-
 
         preguntas.add(new Pregunta("¿Dónde se disputó la Copa Mundial de 1998?",
                 new String[]{
@@ -78,6 +72,7 @@ public class cuestionario extends AppCompatActivity {
         txtPreguntaNumero = findViewById(R.id.txtPreguntaNumero);
         txtPreguntaCompleta = findViewById(R.id.txtPreguntaCompleta);
         textViewTuPuntaje = findViewById(R.id.textViewTuPuntaje);
+
         btnConfirmar = findViewById(R.id.btnConfirmar);
         Intent i = getIntent();
         nombreRecibido = i.getStringExtra("mensaje_key");
@@ -90,123 +85,44 @@ public class cuestionario extends AppCompatActivity {
 
 
     public void mostrarPregunta(){
-
-        if(){
-
+        if(nPregunta<preguntas.size()){
+            Pregunta preguntaActual = preguntas.get(nPregunta);
+            txtPreguntaNumero.setText("Pregunta " + (nPregunta + 1));
+            txtPreguntaCompleta.setText(preguntaActual.getPregunta());
+            String[] opciones = preguntaActual.getOpciones();
+            rButton1.setText(opciones[0]);
+            rButton2.setText(opciones[1]);
+            rButton3.setText(opciones[2]);
+            rButton4.setText(opciones[3]);
+            rButton1.setChecked(false);
+            rButton2.setChecked(false);
+            rButton3.setChecked(false);
+            rButton4.setChecked(false);
+        }else{
+            Intent i = new Intent(this, actividad2.class);
+            i.putExtra("mensaje_key",nombreRecibido);
+            i.putExtra("puntos",puntajeFinal);
+            startActivity(i);
         }
     }
 
 
-
     public void avanzar(View view){
-
-        if(rButton1.isChecked() == false && rButton2.isChecked() == false &&
-                rButton3.isChecked() == false && rButton4.isChecked() == false){
-            Toast.makeText(this,"Elija una opcion ",Toast.LENGTH_SHORT).show();
-
-        }else if(nPregunta==1){
-            if(rButton1.isChecked()){
-                puntajeFinal=puntajeFinal+1;
+        if (rButton1.isChecked() || rButton2.isChecked() || rButton3.isChecked() || rButton4.isChecked()) {
+            Pregunta preguntaActual = preguntas.get(nPregunta);
+            if (
+                    (rButton1.isChecked() && preguntaActual.getRespuestaCorrecta() == 0) ||
+                    (rButton2.isChecked() && preguntaActual.getRespuestaCorrecta() == 1) ||
+                    (rButton3.isChecked() && preguntaActual.getRespuestaCorrecta() == 2) ||
+                    (rButton4.isChecked() && preguntaActual.getRespuestaCorrecta() == 3)) {
+                puntajeFinal++;
             }
-
+            nPregunta++;
+            mostrarPregunta();
             textViewTuPuntaje.setText("Tus Puntos: " + puntajeFinal);
-
-
-            nPregunta = nPregunta+1;
-            txtPreguntaNumero.setText("Pregunta 2");
-            txtPreguntaCompleta.setText("¿Cómo se llamó la mascota de la Copa Mundial de 1982 en España?");
-            rButton1.setText("Juanito");
-            rButton2.setText("Fuleco");
-            rButton3.setText("Pique");
-            rButton4.setText("Naranjito");
-
-            rButton1.setChecked(false);
-            rButton2.setChecked(false);
-            rButton3.setChecked(false);
-            rButton4.setChecked(false);
-
-        }else if(nPregunta==2){
-            if(rButton4.isChecked()){
-                puntajeFinal=puntajeFinal+1;
-            }
-
-            textViewTuPuntaje.setText("Tus Puntos: " + puntajeFinal);
-
-            nPregunta = nPregunta+1;
-
-            txtPreguntaNumero.setText("Pregunta 3");
-            txtPreguntaCompleta.setText("¿En qué año se celebró la primera " +
-                    "Copa Mundial de Fútbol?");
-
-            rButton1.setText("1930");
-            rButton2.setText("1928");
-            rButton3.setText("1942");
-            rButton4.setText("1954");
-
-            rButton1.setChecked(false);
-            rButton2.setChecked(false);
-            rButton3.setChecked(false);
-            rButton4.setChecked(false);
-
-
-        }else if(nPregunta==3){
-            if(rButton1.isChecked()){
-                puntajeFinal=puntajeFinal+1;
-            }
-
-            textViewTuPuntaje.setText("Tus Puntos: " + puntajeFinal);
-
-            nPregunta = nPregunta+1;
-
-            txtPreguntaNumero.setText("Pregunta 4");
-            txtPreguntaCompleta.setText("¿Dónde se disputó la Copa Mundial de 1998?");
-
-            rButton1.setText("Italia");
-            rButton2.setText("Francia");
-            rButton3.setText("Estados Unidos");
-            rButton4.setText("Alemania");
-
-            rButton1.setChecked(false);
-            rButton2.setChecked(false);
-            rButton3.setChecked(false);
-            rButton4.setChecked(false);
-
-
-        }else if(nPregunta==4){
-            if(rButton2.isChecked()){
-                puntajeFinal=puntajeFinal+1;
-            }
-
-            textViewTuPuntaje.setText("Tus Puntos: " + puntajeFinal);
-
-            nPregunta = nPregunta+1;
-
-            txtPreguntaNumero.setText("Pregunta 5");
-            txtPreguntaCompleta.setText("¿Quién fue el jugador que marcó el gol que le dio el triunfo a " +
-                    "España en la final de la Copa del Mundo del 2010 en Sudáfrica?");
-
-            rButton1.setText("Andrés Iniesta");
-            rButton2.setText("David Villa");
-            rButton3.setText("Raúl González");
-            rButton4.setText("Sergio Ramos");
-
-            rButton1.setChecked(false);
-            rButton2.setChecked(false);
-            rButton3.setChecked(false);
-            rButton4.setChecked(false);
-
-        }else if(nPregunta==5){
-            if(rButton1.isChecked()){
-                puntajeFinal=puntajeFinal+1;
-            }
-                Intent i = new Intent(this, actividad2.class);
-                i.putExtra("mensaje_key",nombreRecibido);
-                i.putExtra("puntos",puntajeFinal);
-                startActivity(i);
-
-
+        } else {
+            Toast.makeText(this, "Elija una opción", Toast.LENGTH_SHORT).show();
         }
-
 
 
     }
